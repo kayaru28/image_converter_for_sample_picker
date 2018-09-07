734086420476a1e9e273bfe3442280bb6bdc2e93
kayaru <istorytale090415@gmail.com>
2018-09-07 21:20:14 +0900

    re parallelizing

--- image_converter_for_sample_picker.py
+++ image_converter_for_sample_picker.py
@@ -31,42 +31,49 @@ def echo_process_to(str):
     print("process to " + str)
 
 def get_log_message(count_dir,count_image_file,base_time_0,base_time_n):
-
-    elapsed_time_n = kstd.get_elapsed_time(base_time_n)
-    elapsed_time_n_m = int(elapsed_time_n)
-    time_bet_n = "{0:04d}".format(elapsed_time_n_m)
-
-    elapsed_time_0 = kstd.get_elapsed_time(base_time_0)
-    elapsed_time_0_m = int(elapsed_time_0)
-    time_bet_0 = "{0:04d}".format(elapsed_time_0_m)
+    
+    elapsed_time_n     = kstd.get_elapsed_time(base_time_n,"s")
+    elapsed_time_n_int = int(elapsed_time_n)
+    elapsed_time_n_str = "{0:04d}".format(elapsed_time_n_int)
+    
+    elapsed_time_0   = kstd.get_elapsed_time(base_time_0)
+    elapsed_time_0_int = int(elapsed_time_0)
+    elapsed_time_0_str = "{0:04d}".format(elapsed_time_0_int)
 
 
     message = str(kstd.get_yyyymmddhhmmss())
     message = message + "\tdir:" + "{0:03d}".format(count_dir)
     message = message + "\tfile:" + "{0:07d}".format(count_image_file)
-    message = message + "\t(" + time_bet_n + " / " + time_bet_0 + " m)"
+    message = message + "\t(" + elapsed_time_n_str + " s / " + elapsed_time_0_str + " m)"
     return message
 
+def get_sleep_message(count_dir):
+    message = str(kstd.get_yyyymmddhhmmss())
+    message = message + "\tdir:" + "{0:03d}".format(count_dir)
+    message = message + "\tI am sleeping......"
+    return message
+
+
 def get_image_list_file_path(dir_number,output_number):
-    file_dir         = kstd.get_script_dir()
-    part_name        = str(kstd.get_yyyymmdd()) + "_no-" + "{0:02d}".format(dir_number) 
-    part_name        = part_name + "-" + "{0:02d}".format(output_number) + ".csv"
+    file_dir  = kstd.get_script_dir()
+    part_name = str(kstd.get_yyyymmdd()) + "_no-" + "{0:02d}".format(dir_number) 
+    part_name = part_name + "-" + "{0:02d}".format(output_number) + ".csv"
     file_name = "_image_" + part_name
     file_path = os.path.join(file_dir,file_name)
     return file_path
 
 def get_name_list_file_path(dir_number,output_number):
-    file_dir         = kstd.get_script_dir()
-    part_name        = str(kstd.get_yyyymmdd()) + "_no-" + "{0:02d}".format(dir_number) 
-    part_name        = part_name + "-" + "{0:02d}".format(output_number) + ".csv"
+    file_dir  = kstd.get_script_dir()
+    part_name = str(kstd.get_yyyymmdd()) + "_no-" + "{0:02d}".format(dir_number) 
+    part_name = part_name + "-" + "{0:02d}".format(output_number) + ".csv"
     file_name = "_name_list_" + part_name
     file_path = os.path.join(file_dir,file_name)
     return file_path
 
 def get_log_file_path(dir_number):
-    file_dir         = kstd.get_script_dir()
-    file_name    = "_log_" + str(kstd.get_yyyymmdd()) + "_no-" + "{0:02d}".format(dir_number) + ".csv"
-    file_path    = os.path.join(file_dir,file_name)
+    file_dir  = kstd.get_script_dir()
+    file_name = "_log_" + str(kstd.get_yyyymmdd()) + "_no-" + "{0:02d}".format(dir_number) + ".csv"
+    file_path = os.path.join(file_dir,file_name)
     return file_path
 
 def exec_unit_process_for_create_output_file(directory,dir_number):
@@ -138,7 +145,11 @@ def exec_unit_process_for_create_output_file(directory,dir_number):
             base_time_n = kstd.get_time()
 
         if(count_image_file % prop_values.cycle_sleep == 0):
+            message = get_sleep_message(dir_number)
+            print(message)
             kstd.exec_sleep( prop_values.time_sleep )
+            base_time_n = kstd.get_time()
+
 
     csv_writer_image_list.write_of_array2d(dto_image_list.get_list())
     csv_writer_image_list.close_file()
@@ -154,9 +165,10 @@ def create_output_file(output_file_path):
     # get image flatten list
     # paralell
     len_di = len(directories)
-    #result = jl.Parallel(n_jobs=-1)([jl.delayed(exec_unit_process_for_create_output_file)(directories[di],di) for di in range(len_di)])
-    for di in range(len_di):
-        exec_unit_process_for_create_output_file(directories[di],di)
+
+    result = jl.Parallel(n_jobs=-1)([jl.delayed(exec_unit_process_for_create_output_file)(directories[di],di) for di in range(len_di)])
+    #for di in range(len_di):
+    #    exec_unit_process_for_create_output_file(directories[di],di)
 
 
 if __name__ == "__main__":
